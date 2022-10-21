986c501f2a5c3ae3990cf33fcd4836d71b161ba2
Aisabor <aisabordamisa@outlook.com>
2022-10-22 00:45:20 +0100

    personal-site

---
+++ script.js
@@ -0,0 +1,19 @@
+//JQuery for my click and drop//
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flipother").click(function(){
+      $("#pan").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flipthird").click(function(){
+      $("#pane").slideToggle("slow");
+    });
+  });
